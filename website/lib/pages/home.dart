14a555e38529cd4321397e22bf440a33d8a12c57
ungarmichael <ungarmichael.mail@gmail.com>
2021-12-06 23:42:31 +0100

    lois

--- website/lib/pages/home.dart
+++ website/lib/pages/home.dart
@@ -5,25 +5,40 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(children: <Widget>[
-      Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("assets/idol-sketch.png"),
-            fit: BoxFit.fitHeight,
-            alignment: Alignment.centerRight,
+    return Scaffold(
+      body: Stack(children: <Widget>[
+        Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/idol-sketch.png"),
+              fit: BoxFit.fitHeight,
+              alignment: Alignment.centerRight,
+            ),
           ),
         ),
-      ),
-      Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("assets/idol-sketch.png"),
-            fit: BoxFit.fitHeight,
-            alignment: Alignment.centerLeft,
+        Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Text(
+            'Blogs',
+            textAlign: TextAlign.left,
+            style: const TextStyle(fontWeight: FontWeight.bold),
           ),
-        ),
-      ),
-    ]);
+          Container(
+              child: Row(children: const [
+            Text("Icon", style: TextStyle(fontSize: 25)),
+            Text("Projects", style: TextStyle(fontSize: 25))
+          ])),
+          Text(
+            'Projects',
+            textAlign: TextAlign.left,
+            style: const TextStyle(fontWeight: FontWeight.bold),
+          ),
+          Text(
+            'Team',
+            textAlign: TextAlign.left,
+            style: const TextStyle(fontWeight: FontWeight.bold),
+          ),
+        ])
+      ]),
+    );
   }
 }
