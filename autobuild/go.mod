c7230bca8ab4febde249eeaef31e1166459d7789
ungarmichael <ungarmichael.mail@gmail.com>
2021-12-29 12:32:23 +0100

    droddl

--- autobuild/go.mod
+++ autobuild/go.mod
@@ -2,7 +2,4 @@ module github.com/engelbyte-s-waterbyte/WBYT-blog/autobuild
 
 go 1.15
 
-require (
-	github.com/fsnotify/fsnotify v1.5.1
-	github.com/go-fsnotify/fsnotify v0.0.0-20180321022601-755488143dae // indirect
-)
+require github.com/fsnotify/fsnotify v1.5.1
--- blog-post-cli/main.go
+++ blog-post-cli/main.go
@@ -1,32 +1,51 @@
 package main
 
 import (
+	"bytes"
+	"errors"
 	"flag"
+	"io"
 	"io/ioutil"
 	"log"
+	"mime/multipart"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 )
 
 type blogPost struct {
-	Title         string `json:"title"`
-	Post          string `json:"post"`
-	Creator       string `json:"creator"`
-	ThumbnailPath string `json:"thumbnail"`
+	Title         string
+	Preview       string
+	Post          string
+	ThumbnailPath string
 }
 
 func main() {
+	var username string
+	var password string
 	var newBlogPost blogPost
 	var filePath string
+	var thumbnailPath string
+	flag.StringVar(&username, "username", "", "Enter username")
+	flag.StringVar(&password, "password", "", "Enter password")
 	flag.StringVar(&filePath, "path", "", "Enter path to markdown file containing blog post content")
 	flag.StringVar(&newBlogPost.Title, "title", "", "Enter blog post title")
-	flag.StringVar(&newBlogPost.Creator, "creator", "", "Enter blog post creator")
-	flag.StringVar(&newBlogPost.ThumbnailPath, "thumbnail", "", "Enter path to blog post thumbnail")
+	flag.StringVar(&newBlogPost.Preview, "preview", "", "Enter preview text")
+	flag.StringVar(&thumbnailPath, "thumbnail", "", "Enter path to blog post thumbnail")
 	flag.Parse()
 
-	if filePath == "" || newBlogPost.Title == "" || newBlogPost.Creator == "" || newBlogPost.ThumbnailPath == "" {
+	if filePath == "" || newBlogPost.Title == "" || newBlogPost.ThumbnailPath == "" || newBlogPost.Preview == "" || username == "" || password == "" {
 		flag.Usage()
 		return
 	}
+	var err error
 
+	newBlogPost.ThumbnailPath, err = postImage(thumbnailPath, username, password)
+
+	if err != nil {
+		log.Fatal("Error posting thumbnail", err)
+	}
 	fileContent, err := ioutil.ReadFile(filePath)
 	if err != nil {
 		log.Fatal("Error reading content file:", err)
@@ -34,4 +53,32 @@ func main() {
 	newBlogPost.Post = string(fileContent)
 
 	// TODO
+}
+func postImage(path, username, password string) (string, error) {
+
+	file, err := os.Open(path)
+	if err != nil {
+		return "", err
+	}
+	body := &bytes.Buffer{}
+	writer := multipart.NewWriter(body)
+	part, _ := writer.CreateFormFile("file", filepath.Base(file.Name()))
+	io.Copy(part, file)
+	upart, _ := writer.CreateFormField("username")
+	io.WriteString(upart, username)
+	ppart, _ := writer.CreateFormField("password")
+	io.WriteString(ppart, password)
+	writer.Close()
+	r, _ := http.NewRequest("POST", "http://waterbyte.bplayed.net/blog-post-api/upload-asset.php", body)
+	client := http.Client{}
+	resp, err := client.Do(r)
+	if err != nil {
+		return "", err
+	}
+	b, _ := ioutil.ReadAll(resp.Body)
+	body2 := string(b)
+	if !strings.HasPrefix(body2, "File:") {
+		return "", errors.New(body2)
+	}
+	return body2[5:], err
 }
