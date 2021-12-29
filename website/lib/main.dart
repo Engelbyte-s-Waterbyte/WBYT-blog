a42ac2abeedfb3124282e8554946b07f7a75cc5a
Tobias Wimmer <tobiwimma04@gmail.com>
2021-12-29 16:15:51 +0100

    ok

--- website/lib/main.dart
+++ website/lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-
-import 'app.dart';
+import 'package:url_strategy/url_strategy.dart';
+import 'package:website/app.dart';
 
 void main() {
+  setPathUrlStrategy();
   runApp(const WaterbyteApp());
 }
--- website/lib/pages/not_found.dart
+++ website/lib/pages/not_found.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_tabler_icons/flutter_tabler_icons.dart';
+import 'package:website/components/base_layout.dart';
 
 class NotFound extends StatelessWidget {
   const NotFound({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return BaseLayout(
+      quote: "",
+      child: Image.asset("assets/idol-sketch.png"),
+      heading: "Diese Seite existiert nicht",
+      subHeading:
+          "Bert konnte trotz allen Mühen, Schweiß, Blut und Tränen die Seite, die Sie angefordert haben nicht finden.",
+      headingIcon: TablerIcons.eyeglass,
+    );
   }
 }
