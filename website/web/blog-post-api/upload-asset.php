67b2504a01d8f0194538493f98a5a4e9a181c2c9
Tobias Wimmer <tobiwimma04@gmail.com>
2021-12-29 11:32:14 +0100

    ok

--- website/web/blog-post-api/upload-asset.php
+++ website/web/blog-post-api/upload-asset.php
@@ -4,10 +4,12 @@ require('auth.inc.php');
 
 $target_dir = '../blog-post-assets/';
 $imageFileType = strtolower(pathinfo($_FILES["file"]["name"],PATHINFO_EXTENSION));
-$target_file = $target_dir . uniqid() . $imageFileType;
+$target_file = $target_dir . uniqid() . '.' . $imageFileType;
 $uploadOk = 1;
 
-if ($_FILES)
+if ($_FILES["file"]["size"] > 5000000) {
+    die('File is too large');
+}
 
 move_uploaded_file($_FILES["file"]["tmp_name"], $target_file);
 
