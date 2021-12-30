a34864b9f0f142824725737b5f53bfc61e18cedf
Tobias Wimmer <tobiwimma04@gmail.com>
2021-12-30 15:10:02 +0100

    ok

--- website/web/blog-post-api/upload-post.php
+++ website/web/blog-post-api/upload-post.php
@@ -12,6 +12,6 @@ $new_blog_post = array(
 
 $blog_posts_path = '../resources/blog-posts.json';
 $blog_posts = json_decode(file_get_contents($blog_posts_path), true);
-array_push($blog_posts["blog-posts"], $new_blog_post);
+array_unshift($blog_posts["blog-posts"], $new_blog_post);
 file_put_contents($blog_posts_path, json_encode($blog_posts, JSON_PRETTY_PRINT));
 die('success');
